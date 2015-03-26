4faa1d66b9b1898e3bdf0683cd1b44803c7ea192
Alexander Klintsov <alexklints@yandex-team.ru>
2015-03-26 13:35:48 +0300

    hello world

--- YandexMetricaWorkshopSender/src/main/java/ru/yandex/metricaworkshop/sender/MainFragment.java
+++ YandexMetricaWorkshopSender/src/main/java/ru/yandex/metricaworkshop/sender/MainFragment.java
@@ -12,48 +12,13 @@ import android.widget.Toast;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainFragment extends Fragment implements View.OnClickListener {
-
-    private Spinner mAgeSpinner;
-    private Spinner mGenderSpinner;
-    private Spinner mStateOfEducationSpinner;
-    private Spinner mWorkStatusSpinner;
-    private Spinner mAbsenceReasonSpinner;
-    private Spinner mRatingSpinner;
-
-    private Button mSendUserInfoButton;
-    private Button mSendRatingButton;
+public class MainFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        mAgeSpinner = (Spinner) rootView.findViewById(R.id.age);
-        mGenderSpinner = (Spinner) rootView.findViewById(R.id.gender);
-        mStateOfEducationSpinner = (Spinner) rootView.findViewById(R.id.stage_of_education);
-        mWorkStatusSpinner = (Spinner) rootView.findViewById(R.id.work_status);
-        mAbsenceReasonSpinner = (Spinner) rootView.findViewById(R.id.absence_reason);
-        mRatingSpinner = (Spinner) rootView.findViewById(R.id.workshop_rating);
-
-        mSendUserInfoButton = (Button) rootView.findViewById(R.id.send_user_info);
-        mSendRatingButton = (Button) rootView.findViewById(R.id.send_workshop_rating);
-
-        mSendUserInfoButton.setOnClickListener(this);
-        mSendRatingButton.setOnClickListener(this);
-
         return rootView;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.send_user_info:
-                Toast.makeText(getActivity(), "send_user_info button clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.send_workshop_rating:
-                Toast.makeText(getActivity(), "send_workshop_rating button clicked", Toast.LENGTH_SHORT).show();
-                break;
-        }
     }
 }
